8b1262c07b5ec0fe3b039076e8b09fd3cfda70b1
antemordacity <ramjyothy9@gmail.com>
2026-09-03 02:31:10 +0530

    11x11 and 13x13 tested

--- task1/src/conv_unroll.cpp
+++ task1/src/conv_unroll.cpp
@@ -8,9 +8,9 @@ void conv_unroll(const float* in, float* out, const float* ker,
     int kx = 0;
     for (int oy = 0; oy < H; ++oy) {
         for (int ox = 0; ox < W; ++ox) {
-            float acc0 = 0.0f, acc1 = 0.0f, acc2 = 0.0f, acc3 = 0.0f, acc4 = 0.0f, acc5 = 0.0f, acc6 = 0.0f, acc7 = 0.0f, acc8 = 0.0f;
+            float acc0 = 0.0f, acc1 = 0.0f, acc2 = 0.0f, acc3 = 0.0f, acc4 = 0.0f, acc5 = 0.0f, acc6 = 0.0f, acc7 = 0.0f, acc8 = 0.0f, acc9 = 0.0f, acc10 = 0.0f, acc11 = 0.0f, acc12 = 0.0f;
             for (int ky = 0; ky < K; ++ky) {
-                for (kx = 0; kx < K - 8; kx+=9) {
+                for (kx = 0; kx < K - 12; kx+=13) {
                     acc0 += in[(oy + ky) * in_stride + (ox + kx)] * ker[ky * K + kx];
                     acc1 += in[(oy + ky) * in_stride + (ox + kx + 1)] * ker[ky * K + kx + 1];
                     acc2 += in[(oy + ky) * in_stride + (ox + kx + 2)] * ker[ky * K + kx + 2];
@@ -20,12 +20,16 @@ void conv_unroll(const float* in, float* out, const float* ker,
                     acc6 += in[(oy + ky) * in_stride + (ox + kx + 6)] * ker[ky * K + kx + 6];
                     acc7 += in[(oy + ky) * in_stride + (ox + kx + 7)] * ker[ky * K + kx + 7];
                     acc8 += in[(oy + ky) * in_stride + (ox + kx + 8)] * ker[ky * K + kx + 8];
+                    acc9 += in[(oy + ky) * in_stride + (ox + kx + 9)] * ker[ky * K + kx + 9];
+                    acc10 += in[(oy + ky) * in_stride + (ox + kx + 10)] * ker[ky * K + kx + 10];
+                    acc11 += in[(oy + ky) * in_stride + (ox + kx + 11)] * ker[ky * K + kx + 11];
+                    acc12 += in[(oy + ky) * in_stride + (ox + kx + 12)] * ker[ky * K + kx + 12];
                 }
                 for(; kx < K; ++kx){
                     acc0 += in[(oy + ky) * in_stride + (ox + kx)] * ker[ky * K + kx];
                 }
             }
-            out[oy * W + ox] = acc0 + acc1 + acc2 + acc3 + acc4 + acc5 + acc6 + acc7 + acc8;
+            out[oy * W + ox] = acc0 + acc1 + acc2 + acc3 + acc4 + acc5 + acc6 + acc7 + acc8 + acc9 + acc10 + acc11 + acc12;
         }
     }
 }
