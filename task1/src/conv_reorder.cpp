9fc67f88a0a0af2b68f79240f10b4b0f16c09708
shullies <anshulgyl06@gmail.com>
2026-09-06 23:50:50 +0530

    stuff

--- task1/src/conv_reorder.cpp
+++ task1/src/conv_reorder.cpp
@@ -2,20 +2,28 @@
 // Hint: loops from outermost to innermost -> ky, kx, oy, ox.
 
 #include "convolution.h"
+
 void conv_reorder(const float* in, float* out, const float* ker,
                   int H, int W, int K) {
-                    const int p = K / 2;
-    const int in_stride = W + 2 * p;  // padded row stride
+    const int p = K / 2;
+    const int in_stride = W + 2 * p;
+
     for (int i = 0; i < H * W; ++i)
         out[i] = 0.0f;
-    for (int ky = 0; ky < K; ++ky) {
-        for (int kx = 0; kx < K; ++kx) {
-            float k = ker[ky * K + kx];
-            for (int oy = 0; oy < H; ++oy) {
+
+    for (int oy = 0; oy < H; ++oy) {
+        for (int ky = 0; ky < K; ++ky) {
+            for (int kx = 0; kx < K; ++kx) {
+
+                float kval = ker[ky * K + kx];
+
                 for (int ox = 0; ox < W; ++ox) {
-                    out[oy * W + ox] += in[(oy + ky) * in_stride + (ox + kx)] * k;
+                    out[oy * W + ox] +=
+                        in[(oy + ky) * in_stride + (ox + kx)]
+                        * kval;
                 }
             }
         }
     }
 }
+
